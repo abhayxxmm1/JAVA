f0ef96eea898491b919e180655c0dd444556af11
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2025-03-20 23:29:25 +0530

    Introduction of myself using java basic syntax

---
+++ 01_basics/Program1.java
@@ -0,0 +1,13 @@
+class Program1
+{
+
+public static void main (String[]args)
+	{
+		System.out.println("Name : Abhay Sanjay Ghongade") ;
+		System.out.println("College : Godavari college of engineering ") ;
+		System.out.println("PRN : 2151711245042") ;
+		System.out.println("DOB : 20/04/2004") ;
+
+	}
+
+}
