df65670f79fa8bd2de859aedc3ae95ccf090462d
Abhay <ghongadeabhay60@gmail.com>
2025-03-21 01:11:41 +0530

    * writing code using sublime text. and saving it as .java file * also understood all the basic CLI(command line interface) terminal commands. * to compile .java file to .class file and run the byte_code using jvm. * jvm is inside--> JRE(java runtime environment) inside--> JDK(java development kit). * every time we update need to save and then compile it to .class to generate byte_code. * after that run using command --> java Program1  inside the terminal.

--- 01_basics/Program1.java
+++ 01_basics/Program1.java
@@ -3,10 +3,11 @@ class Program1
 
 public static void main (String[]args)
 	{
-		System.out.println("Name : Abhay Sanjay Ghongade") ;
-		System.out.println("College : Godavari college of engineering ") ;
-		System.out.println("PRN : 2151711245042") ;
-		System.out.println("DOB : 20/04/2004") ;
+		System.out.println("Name: Abhay Sanjay Ghongade") ;
+		System.out.println("College: Godavari college of engineering ") ;
+		System.out.println("Marital Status: Divorsed") ;
+		System.out.println("Age: 0") ;
+		System.out.println("Salary: 5000.50") ;
 
 	}
 
