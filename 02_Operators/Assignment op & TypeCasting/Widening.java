b42a165ad72de63b1d48136d7c25fdd6fd2064e0
Abhay <ghongadeabhay60@gmail.com>
2025-03-27 23:26:00 +0530

    Widening: coverting or storing smaller data type to larger data type, it is implicitely done by the compiler.

---
+++ 02_Operators/Assignment op & TypeCasting/Widening.java
@@ -0,0 +1,15 @@
+class Widening 
+{
+	public static void main (String[] args)
+	{
+		int i = 'a';
+		System.out.println(i); // 97
+
+		double d = 1023 ;
+		System.out.println(d); // 1023.0
+
+		float f = 'z';
+		System.out.println (f);  // 122.0
+
+	}
+}
