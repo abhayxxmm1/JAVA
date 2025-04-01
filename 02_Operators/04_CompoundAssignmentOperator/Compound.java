171f4b2bafa9dd9fe0be542e8796ece8d99480a7
Abhay <ghongadeabhay60@gmail.com>
2025-04-01 18:29:49 +0530

    compound assignment operator syntax is better and faster also it can perform Implicite narrowing

---
+++ 02_Operators/04_CompoundAssignmentOperator/Compound.java
@@ -0,0 +1,14 @@
+class Compound
+{
+	public static void main (String[] args)
+	{
+		int sal = 5000;
+		// sal = ( sal*2000 )
+		sal *= 2000;
+		System.out.println(sal);	//
+
+		// We can do Implicite narrowing in java
+		// CAO can perform Implicite narrowing
+		// means compiler can do narroeing by itself
+	}	
+}
