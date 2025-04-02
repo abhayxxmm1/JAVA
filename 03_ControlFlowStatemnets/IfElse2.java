2d0c0ab555aa51175f162117304365fd67efedaf
Abhay <ghongadeabhay60@gmail.com>
2025-04-03 01:19:58 +0530

    Program to check if a number is +ve or -ve

---
+++ 03_ControlFlowStatemnets/IfElse2.java
@@ -0,0 +1,16 @@
+class IfElse2
+{
+	public static void main (String[] args)
+	{
+		int num = -19;
+
+		if (num<0)
+		{
+			System.out.println("It is negative");
+		}
+		else
+		{
+			System.out.println("It is positve");
+		}
+	}
+}
