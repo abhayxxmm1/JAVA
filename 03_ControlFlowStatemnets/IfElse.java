43de71c4d7d8c79c48994682cb9d0cf600c42f61
Abhay <ghongadeabhay60@gmail.com>
2025-04-03 01:14:21 +0530

    To check wether the number is odd or even

---
+++ 03_ControlFlowStatemnets/IfElse.java
@@ -0,0 +1,16 @@
+class IfElse
+{
+	public static void main (String[] args)
+	{
+		int num = 230; // num =23;
+
+		if(num%2==0)
+		{
+			System.out.println("It is even");
+		}
+		else
+		{
+			System.out.println("It is odd");
+		}
+	}
+}
