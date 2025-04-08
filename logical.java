462aba7ad79adad6b17745cf0dabff2730c7d606
Abhay <ghongadeabhay60@gmail.com>
2025-04-08 23:51:22 +0530

    check wether character is alphabet or not

---
+++ logical.java
@@ -0,0 +1,10 @@
+class Logical
+{
+	public static void main(String[] args) {
+		char ch = 'H';
+
+	System.out.println((ch>='A' && ch<='Z')?
+						(ch+ " it's a Uppercase Alpha")
+						:(ch+ " it's not a Uppercase alpha"));
+	}
+}
