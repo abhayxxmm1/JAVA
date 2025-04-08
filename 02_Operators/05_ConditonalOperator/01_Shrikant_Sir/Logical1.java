e91c2fa66d0e414d9062d74fa08caab396c8ea03
Abhay <ghongadeabhay60@gmail.com>
2025-04-08 23:58:14 +0530

    program to check character is digit or not

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/Logical1.java
@@ -0,0 +1,10 @@
+class Logical1
+{
+	public static void main(String[] args) {
+		char ch = '1';
+
+	System.out.println((ch>='0' && ch<='9')?
+						(ch+ " it's a Digit Alpha")
+						:(ch+ " it's not a Digit alpha"));
+	}
+}
