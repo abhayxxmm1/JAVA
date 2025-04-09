9b2d409f0be226a691bee6cd1e76bbea58962059
Abhay <ghongadeabhay60@gmail.com>
2025-04-09 23:26:54 +0530

    All about final concept

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/FinalConcept.java
@@ -0,0 +1,23 @@
+class FinalConcept
+{
+	public static void main(String[] args) {
+		int a = 10;
+		a = 20;
+
+		System.out.println(a);
+
+		// final is a keyWord and modifier 
+		// can be used in class, variable as final it cannot be re-intialized
+		// final variable are treated as COSTANTS & we cannot change them
+
+		final int b = 1;
+		b = 20;
+		System.out.println(b);
+
+		final double PI = 3.14;
+		final double GRAVITATIONAL_FORCE = 9.81; 
+		// while declaring final variable we should follow 
+		// the rules of identifier along with convention(ALL CHARACTER UPPERCASE)
+		
+	}
+}
