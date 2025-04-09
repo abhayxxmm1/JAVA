70a2438b1c9888eeddf8e2b29e41efb6387c5308
Abhay <ghongadeabhay60@gmail.com>
2025-04-09 23:29:42 +0530

    Min & Max of Data type using Rapper class variable

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/MinMaxOfDataType.java
@@ -0,0 +1,28 @@
+class MinMaxOfDataType
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("min : "+Byte.MIN_VALUE);
+		System.out.println("min : "+Byte.MAX_VALUE); // 3 digit
+
+		System.out.println("min : "+Short.MIN_VALUE);
+		System.out.println("min : "+Short.MAX_VALUE); // 5 digit
+
+		System.out.println("min : "+Integer.MIN_VALUE);
+		System.out.println("min : "+Integer.MAX_VALUE); // 9 digit
+
+		System.out.println("min : "+Long.MIN_VALUE);
+		System.out.println("min : "+Long.MAX_VALUE); // 19 digit
+
+		System.out.println("min : "+(Character.MIN_VALUE+0));
+		System.out.println("min : "+(Character.MAX_VALUE+0));
+
+		System.out.println("min : "+Float.MIN_VALUE);
+		System.out.println("min : "+Float.MAX_VALUE); // 39 digit
+
+		System.out.println("min : "+Double.MIN_VALUE);
+		System.out.println("min : "+Double.MAX_VALUE); // 309 digit
+
+
+	}
+}
