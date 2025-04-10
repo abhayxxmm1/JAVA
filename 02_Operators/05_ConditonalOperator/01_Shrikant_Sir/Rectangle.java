7b0bf619b9b36bc1eb3ca0bb91780ced25d95af2
Abhay <ghongadeabhay60@gmail.com>
2025-04-11 01:22:02 +0530

    Area and Perimeter of Rectangle

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/Rectangle.java
@@ -0,0 +1,14 @@
+class Rectangle
+{
+	public static void main(String[] args) 
+	{
+		float width = 4.5f, height = 7.9f;
+
+		float area = width*height;
+		System.out.println("area of rectangle is "+area);
+
+		float perimeter = 2*(width + height);
+		System.out.println("perimeter of rectangle is "+perimeter);
+
+	}
+}
