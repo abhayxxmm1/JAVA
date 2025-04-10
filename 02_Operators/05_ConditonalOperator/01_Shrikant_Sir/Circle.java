7edbd18b6c38d6fcda08690b4726965706220c23
Abhay <ghongadeabhay60@gmail.com>
2025-04-11 00:26:46 +0530

    Area and Perimeter of circle using Final keyword and Float (not double bcoz it is too large 309 digits)

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/Circle.java
@@ -0,0 +1,15 @@
+class Circle
+{
+	public static void main(String[] args) 
+	{
+		float radius = 5.5f ; // need to write f bcoz double is by default considered
+		final float PI = 3.147f ;
+		float areaOfCircle = PI * radius * radius ;
+
+		System.out.println("Area of circle for radius : "+radius+
+							"cm, is "+areaOfCircle+"cm^2");
+
+		float perimeterOfCircle = 2*PI*radius;
+		System.out.println("perimeter of circle : "+ perimeterOfCircle+"cm");
+	}
+}
