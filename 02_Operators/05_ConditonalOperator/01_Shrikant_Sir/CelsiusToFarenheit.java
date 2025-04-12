6f2fe511af83f1afc3a934498cde29d579c24723
Abhay <ghongadeabhay60@gmail.com>
2025-04-13 03:08:45 +0530

    Program to convert from Celcius to Farenheit.

---
+++ 02_Operators/05_ConditonalOperator/01_Shrikant_Sir/CelsiusToFarenheit.java
@@ -0,0 +1,12 @@
+class CelsiusToFarenheit
+{
+	public static void main(String[] args) 
+	{
+		double  celsius = 43;
+
+		double farenheit = (9.0/5)*celsius + 32;
+
+		System.out.println("Enter a degree in celsius: "+celsius);	
+		System.out.println(celsius+" is "+farenheit+" farenheit");	
+	}
+}
