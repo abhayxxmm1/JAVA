4971b188dc9606329b9da52e0210dd415fc43caa
Abhay <ghongadeabhay60@gmail.com>
2025-04-16 17:47:18 +0530

    Simple If statement example without scanner class and method Signature

---
+++ 03_ControlFlowStatemnets/02.1_If_state_shrikantSir/IfExample.java
@@ -0,0 +1,12 @@
+class IfExample
+{
+	public static void main (String [] args)
+	{
+		String response = "cleared";
+		if(response.equals("cleared"))
+		{
+			System.out.println("You have cleared your interview ");
+			System.out.println("Stay updated foor next Round ");
+		}
+	}
+}
