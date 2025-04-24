f03cd0929f354fe7120af3a4f3c11426b13c99dc
Abhay <ghongadeabhay60@gmail.com>
2025-04-24 22:12:23 +0530

    To find the count of given range of  num using while loop and applying condition using if loop.

---
+++ 04_loops_stmt/01_while_loop/Count.java
@@ -0,0 +1,16 @@
+class Count
+{
+	public static void main(String[] args) 
+	{
+		int count =1;
+		while(count<=10000)
+		{	
+			if (count%2==0) 
+			{
+				System.out.println(count);
+			}
+			
+			count++;
+		}	
+	}
+}
