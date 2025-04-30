8e65d5049ad9a6c43ac8004fa43f25f102643b76
Abhay <ghongadeabhay60@gmail.com>
2025-05-01 01:43:04 +0530

    ⭐️ prgm to check if given num is prime no or not in most optimal code.

---
+++ 00_Programming/30_APR_⭐️/PrimeNum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class PrimeNum
+{
+	public static void main (String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the num: ");
+		int num = sc.nextInt();
+		int den = 2;
+		while(den<num/2)
+		{
+			if (num%den==0) 
+			{
+				break;
+			}
+			den++;
+		}
+		if(num/2<=den)
+		{
+			System.out.println("It is prime");
+		}
+		else
+		{
+			System.out.println("Not a prime");
+		}
+	}
+}
