ec2e14db8ba6748b565c5a275b5e81838ae71322
Abhay <ghongadeabhay60@gmail.com>
2025-05-14 01:17:40 +0530

    Nested loop

---
+++ 00_Programming/31_NESTED_LOOP_⭐️/Nested1.java
@@ -0,0 +1,13 @@
+class Nested1
+{
+	public static void main (String[]args)
+	{
+		for (int days = 0; days < 3; days++)
+		{
+			for (int times = 0; times < 5; times++) 
+			{
+				System.out.println("Good Morning");	
+			}
+		}
+	}
+}
