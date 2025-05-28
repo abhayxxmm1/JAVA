d52ec1830c8961fbc76a36e8cc65d555c3602dfa
Abhay <ghongadeabhay60@gmail.com>
2025-05-29 02:36:14 +0530

    Update

--- 07_Recurssion/RecFact.java
+++ 07_Recurssion/RecFact.java
@@ -1,5 +1,11 @@
 class RecFact
 {
+	public static void main(String[] args) 
+	{
+		System.out.println(fact(5));
+		System.out.println(power(5, 3));
+		System.out.println(f(4));
+	}
 	public static int fact(int num)
 	{
 		return num==0 ? 1 : num*fact(num-1);
@@ -9,11 +15,7 @@ class RecFact
 	{
 		return raise==0 ? 1 : base*power(base, raise-1);
 	}
-	public static void main(String[] args) {
-		System.out.println(fact(5));
-		System.out.println(power(5, 3));
-		System.out.println(f(4));
-	}
+	
 	public static int f(int num)
 	{
 		if(num==0)
