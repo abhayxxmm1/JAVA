9e55e774c9103465ab86ff76bf4316548e426395
Abhay <ghongadeabhay60@gmail.com>
2025-06-10 00:38:51 +0530

    update

--- 07_Recurssion/RecFibo.java
+++ 07_Recurssion/RecFibo.java
@@ -2,12 +2,12 @@ class RecFibo
 {
 	public static void fibo(int n, int n1, int n2)
 	{
-		if (n>0)
+		if (n>0) // until n is greatrer than 0
 		{
 			System.out.println(n1);
 			fibo(n-1, n2, n1+n2);
 		}
-		return ;
+		return ;	// empty return used to stop the execution of method by desroying the frame
 	}
 	public static void main(String[] args) {
 		fibo(10, 0, 1);
